94eac87265da8b37254d7716eeacc84610a635bc
Jean-Christophe <jcclerval@aol.com>
2017-01-02 22:48:35 +0100

    maj mqtt

--- rasp/scriptRasp.py
+++ rasp/scriptRasp.py
@@ -58,7 +58,7 @@ def scan(l):
     for i in range(0,l):
         print "Tour ",i
         red = read()
-        if red != None and if red != '':
+        if red != None and red != '':
             temp.append(red)                                                   # On ajoute la valeur de l'étiquette si elle est non nulle
     temp = list(set(temp))                                                     # On transforme la liste pour supprimer les doublons
     if temp != []:
